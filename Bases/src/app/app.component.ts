043e766be8477928c84f63ecd390838a7af524f1
Adrian Camarena <adriancamarena2310mail.com>
2023-11-17 23:16:46 -0600

    video-41 Contador

--- Bases/src/app/app.component.ts
+++ Bases/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   public title: string = 'Mi primera app de Angular';
   public counter: number = 10;
+
+  public increseBy( value:number ):void{
+    this.counter += value;
+  }
+
+  public reset():void{
+    this.counter = 10;
+}
 }
